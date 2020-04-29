f6debc688c0607ae1d33e8bf45922f09e9ba89d9
JeremyMeed <61828307+JeremyMeed@users.noreply.github.com>
2020-04-29 18:31:13 +0200

    Update inscription_bdd

--- inscription_bdd.php
+++ inscription_bdd.php
@@ -6,21 +6,22 @@ require("include/connecbdd.php");
 	{
 		$nom = htmlspecialchars($_POST['nom']);
 		$prenom = htmlspecialchars($_POST['prenom']) ;
-		$username = htmlspecialchars($_POST['username']) ;
+		$username = htmlspecialchars($_POST['username']);
 		$pass_hache = password_hash($_POST['password'], PASSWORD_DEFAULT);
-		$question = ($_POST['choix']) ;
-		$reponse = htmlspecialchars($_POST['reponse']) ;
+		$question = ($_POST['choix']);
+		$reponse = htmlspecialchars($_POST['reponse']);
+		
 		if (!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['username']) AND !empty($_POST['password']) AND !empty($_POST['choix'] AND !empty($_POST['reponse'])))
 		{		
 			//pseudo libre ? 
 			$requsername = $bdd->prepare("SELECT id_user FROM users WHERE username = ?");
-			$requsername->execute(array($_POST['username']));
+			$requsername->execute(array($username));
 			$usernameexist = $requsername->rowcount();
 			if ($usernameexist == 0) 
 			{
 				// insérer dans la base de données 
 				$req = $bdd->prepare('INSERT INTO users(nom, prenom, username, password, question_secrete, reponse_secrete) VALUES(?, ?, ?, ?, ?, ?)');
-				$req->execute(array($_POST['nom'], $_POST['prenom'], $_POST['username'],$pass_hache, $_POST['choix'], $_POST['reponse']));
+				$req->execute(array($nom, $prenom, $username,$pass_hache, $question, $reponse));
 				// Puis redirection du visiteur vers la page de connexion
 				header('Location: page_connexion.php'); 
 			}        
