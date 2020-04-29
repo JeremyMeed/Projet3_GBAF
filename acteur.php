4450c033741e02c6f2f708682ac15253931f2527
JeremyMeed <61828307+JeremyMeed@users.noreply.github.com>
2020-04-29 18:25:12 +0200

    Acteur.php validator W3C

--- acteur.php
+++ acteur.php
@@ -31,19 +31,6 @@ if(isset($_GET['id']) && !empty($_GET['id']))
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php 
-            if(!empty($title))
-            {
-        ?>
-        <title><?= $title; }?></title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-    </head>
-        <body>
             <!-- Presentation acteur -->
             <div class="bloc_description">
                 <div class="bloc_logoacteur">
@@ -119,8 +106,6 @@ if(isset($_GET['id']) && !empty($_GET['id']))
                 $req->closeCursor();
                 ?>
             </div>
-        </body>
-</html>
 <?php 
     require_once('include/footer.php'); 
 ?>
