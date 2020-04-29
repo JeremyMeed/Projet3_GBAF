fe728fe964a8b6f1f146720003323d5b3d963095
JeremyMeed <61828307+JeremyMeed@users.noreply.github.com>
2020-04-29 18:17:26 +0200

    Update footer validator W3C

--- include/footer.php
+++ include/footer.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php 
-            if(!empty($title))
-        {
-        ?>
-        <title><?= $title; }?></title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-    </head>
 <!-- FOOTER -->     
-    <footer>
-    	<div class="container">
-    		<address>
-            	<p>Site créé dans le cadre d'un projet OpenClassrooms |
-            	<a href="mentions.php">Mentions légales</a> |
-            	<a href="mailto:jeremymedana@gmail.com">Contact</a></p>
-            </address>
-        </div>
-    </footer>
+	    <footer>
+	    	<div class="container">
+	    		<address>
+	            	<p>Site créé dans le cadre d'un projet OpenClassrooms |
+	            	<a href="mentions.php">Mentions légales</a> |
+	            	<a href="mailto:jeremymedana@gmail.com">Contact</a></p>
+	            </address>
+	        </div>
+	    </footer>
+	</body>
 </html> 
