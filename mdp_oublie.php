8e75a80e3e5a3ab0bffa939f04dc5fcb4dfa4580
JeremyMeed <61828307+JeremyMeed@users.noreply.github.com>
2020-04-29 19:00:36 +0200

    Update mdp_oublie.php

--- mdp_oublie.php
+++ mdp_oublie.php
@@ -42,19 +42,7 @@ if (isset($_POST['submit']))
     }       
 }
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php 
-            if(!empty($title))
-            {
-        ?>
-        <title><?= $title; }?></title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-    </head>
-    <body>
+
         <!-- Formulaire pseudo et réponse -->
         <div id="login">
             <form class="form" method="post" action="mdp_oublie.php">
@@ -72,8 +60,6 @@ if (isset($_POST['submit']))
             if(isset($champs)) {echo $champs;}
             ?>
         </div>
-    </body>
-</html>
 <?php 
 require_once('include/footer.php');
 ?>   
